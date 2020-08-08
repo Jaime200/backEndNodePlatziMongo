2b488028e1c9b2909025278519bc41b814a1399e
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-08-08 12:49:23 -0600

    Se agrega un error simulado con el parametro de details

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ router.post('/message', function(req,res){
     console.log(req.body);
     console.log(req.query);   
     if(req.query.error == "ok"){
-        response.error(req, res,'Error simulado',400);
+        response.error(req, res,'Error inesperado',500,'Es solo una simulacion de los errores');
     }else{
     // res.status(201).send({error:'', body: 'Creado correctamente' });
     response.success(req, res,'Creado correctamente',201);
