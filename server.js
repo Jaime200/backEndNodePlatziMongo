a71553753670d6cf2c813aea711629138ea243c0
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-08-08 14:11:55 -0600

    Se modifica el archivo de serve.js para importar el router que es el archivo que maneja las rutas

--- server.js
+++ server.js
@@ -1,46 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = express.Router();
-const response = require('./network/response')
+
+
+const router = require('./network/router');
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(router);
-
-router.get('/message', function(req,res){
-    console.log(res.header);
-    res.header({
-        "custom-header": "Nuestro valor personalizado",
-    });
-    //res.send('Lista de mensajes');
-    response.success(req, res,'Lista de mensajes');
-    
-});
-
-router.post('/message', function(req,res){
-    console.log(req.body);
-    console.log(req.query);   
-    if(req.query.error == "ok"){
-        response.error(req, res,'Error inesperado',500,'Es solo una simulacion de los errores');
-    }else{
-    // res.status(201).send({error:'', body: 'Creado correctamente' });
-    response.success(req, res,'Creado correctamente',201);
-    }
-    
-});
-
+router(app);
 
 app.use('/app',express.static('public'))
-
-
-router.delete('/message', function(req,res){
-    console.log(req.body);
-    console.log(req.query)
-    res.status(201).send('Mensaje '+ req.body.text + ' eliminado');
-    
-});
-
-
 
 // app.use('/',function(req,res){
 //     res.send('Hola');
