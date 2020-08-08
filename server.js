4b796662c41cff0d4d86a9e30cb1049039d68c70
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-08-08 09:59:03 -0600

    Se agrego el router de express para manejar las peticiones http

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const router = express.Router();
 let app = express();
 
-app.use('/',function(req,res){
-    res.send('Hola');
 
-    res.end();
-})
+app.use(router);
 
+router.get('/message', function(req,res){
+    res.send('Lista de mensajes');
+    
+});
+
+router.post('/message', function(req,res){
+    res.send('Mensaje añadido');
+    
+});
+
+
+
+// app.use('/',function(req,res){
+//     res.send('Hola');
+//     res.end();
+// })
 
 app.listen(3000);
 console.log('La aplicacion esta escuchando en el puerto 3000');
