f888b93639c3984be377b5abf4726a44b6ee263f
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-08-08 12:48:40 -0600

    Se agrego un parametro de details para saber los detalles del error y se utiliza chalk para mostrar colores en consola

--- network/response.js
+++ network/response.js
@@ -1,4 +1,7 @@
+const chalk = require('chalk');
+
 exports.success = function (req, res, message, status){
+    console.log(chalk.green('[response success]'));
     res.status(status || 200).send({   
         error : '',
         body: message
@@ -7,7 +10,8 @@ exports.success = function (req, res, message, status){
 }
 
 
-exports.error = function (req, res, message, status){
+exports.error = function (req, res, message, status, details){
+    console.error(chalk.red('[response error] ') + details);
     res.status(status || 500).send({   
         error : message,
         body: ''
