bffc1ff2633f07ab645ee0f50d7f3844e5e7ed25
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-08-10 01:58:12 -0600

    Se importa el archivo db.js en el archivo de server, ademas se agrega tambien el archivo de chalk para mostrar mensajes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const chalk = require('chalk');
+const db = require('./db');
 
 const router = require('./network/router');
+
+db('mongodb+srv://jmunozV2:NIUfgdXGxv6mkMzy@notadeventa-mppfw.mongodb.net/chat?retryWrites=true&w=majority')
+
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -16,4 +20,4 @@ app.use('/app',express.static('public'))
 // })
 
 app.listen(3000);
-console.log('La aplicacion esta escuchando en el puerto 3000');
+console.log(chalk.blue('[server.js] La aplicacion esta escuchando en el puerto 3000'));
