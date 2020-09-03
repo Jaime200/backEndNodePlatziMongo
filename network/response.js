6602be89c3097852d018c34377fdc1102d3dbc30
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-09-02 19:45:55 -0600

    Modificacion en response.js

--- network/response.js
+++ network/response.js
@@ -1,10 +1,28 @@
 const chalk = require('chalk');
 
+const statusMessages = {
+    '200' : 'Done',
+    '201' : 'Created',
+    '400' : 'Invalid format',
+    '500' : 'Internal error'
+}
+
+
 exports.success = function (req, res, message, status){
     console.log(chalk.green('[response success]'));
-    res.status(status || 200).send({   
+    let statusCode = status 
+    let statusMessage = message
+    if(!status){
+        status = 200;
+    }
+    
+    if(!message){
+        statusMessage = statusMessages[status]
+    }
+
+    res.status(statusCode).send({   
         error : '',
-        body: message
+        body: statusMessage
     });
 
 }
